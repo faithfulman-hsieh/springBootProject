80af39ad89c95abfb99a115b1522e4e8d2342a5e
謝秉融 <>
2026-01-22 12:35:51 +0800

    增加通話開始跟結束的記錄

--- src/main/java/com/taskmanager/notification/dto/ChatMessage.java
+++ src/main/java/com/taskmanager/notification/dto/ChatMessage.java
@@ -7,7 +7,7 @@ public class ChatMessage {
     private String sender;
     private String receiver;
     private String content;
-    private String type;
+    private String type; // CHAT, JOIN, LEAVE, CALL_START, CALL_END
     private String time;
     private String data; // For WebRTC or other data
 
